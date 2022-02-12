2accbdd3d561c28c3a9586b90f8f4d7fef0d706c
Niels Hoppe <mail@nielshoppe.de>
2022-02-12 16:49:55 +0100

    Remove deprecated code

--- src/commonTest/kotlin/com.dancemesh.common.algorithms.ranking/MajorityRankingAlgorithmUtilsTest.kt
+++ src/commonTest/kotlin/com.dancemesh.common.algorithms.ranking/MajorityRankingAlgorithmUtilsTest.kt
@@ -20,4 +20,53 @@ class MajorityRankingAlgorithmUtilsTest() {
             assertEquals(it.second, MajorityRankingAlgorithmUtils.getMajority(it.first))
         }
     }
+
+/*
+	@Test
+	fun testSumByIntFraction () {
+		val actualResult = MajorityRankingAlgorithmUtils.sumByIntFraction(listOf(
+				IntegerInterval(1),
+				IntegerInterval(2)
+		))
+
+		assertTrue(actualResult.equals(IntegerInterval(3)))
+	}
+	*/
+
+    /*
+    class ScoringAggregator: ArgumentsAggregator {
+
+        override fun aggregateArguments(arguments: ArgumentsAccessor, context: ParameterContext): Scoring<Int, List<IntegerInterval>> {
+
+            var scoring: MutableScoring<Int, List<IntegerInterval>> = ScoringImpl()
+
+             //stackoverflow.com/questions/45086786/cast-any-to-array-in-kotlin
+            val rows = (arguments.toArray() as? Array<*>)?.filterIsInstance<Array<Int>>()
+
+            for (row in rows!!) {
+
+                val id = row[0]
+                scoring.putScore(id, row.drop(2).map({x -> IntegerInterval(x)}).toList())
+            }
+
+            return scoring
+        }
+    }
+
+    class RankingAggregator: ArgumentsAggregator {
+
+        override fun aggregateArguments(arguments: ArgumentsAccessor, context: ParameterContext): Ranking<Int> {
+
+            var ranking: MutableRanking<Int> = RankingImpl()
+
+            for (row in rawData) {
+
+                val id = row[0]
+                ranking.putRank(id, IntegerInterval(row[1]))
+            }
+
+            return ranking
+        }
+    }
+    */
 }
